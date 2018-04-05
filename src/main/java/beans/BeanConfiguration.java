4182569644d965e20f469dac265bad48d4371e14
PamGoehring <goehring.pam@principal.com>
2018-04-04 21:31:20 -0500

    Cleaned Up Code - Comment Bean
    commented out code in original bean files because I didn't want the red Xs but also didn't want to lose the code examples I had.

--- src/main/java/beans/BeanConfiguration.java
+++ src/main/java/beans/BeanConfiguration.java
@@ -1,25 +1,25 @@
-package beans;
-
-import org.springframework.beans.factory.annotation.Autowire;
-import org.springframework.context.annotation.Bean;
-
-public class BeanConfiguration {
-
-	@Bean
-	public CollectionDao collectionDao() {
-		CollectionDaoInMemoryImpl bean = new CollectionDaoInMemoryImpl();
-		return bean;
-	}
-	
-	@Bean(autowire=Autowire.BY_NAME)
-	public CollectionService collectionService() {
-		CollectionServiceImpl bean = new CollectionServiceImpl(collectionDao());
-		return bean;
-	}
-	
-	@Bean
-	public CollectionDao collectionDaoJpa() {
-		CollectionDaoJpaImpl bean = new CollectionDaoJpaImpl();
-		return bean;
-	}
-}
+//package beans;
+//
+//import org.springframework.beans.factory.annotation.Autowire;
+//import org.springframework.context.annotation.Bean;
+//
+//public class BeanConfiguration {
+//
+//	@Bean
+//	public CollectionDao collectionDao() {
+//		CollectionDaoInMemoryImpl bean = new CollectionDaoInMemoryImpl();
+//		return bean;
+//	}
+//	
+//	@Bean(autowire=Autowire.BY_NAME)
+//	public CollectionService collectionService() {
+//		CollectionServiceImpl bean = new CollectionServiceImpl(collectionDao());
+//		return bean;
+//	}
+//	
+//	@Bean
+//	public CollectionDao collectionDaoJpa() {
+//		CollectionDaoJpaImpl bean = new CollectionDaoJpaImpl();
+//		return bean;
+//	}
+//}
--- src/main/java/beans/CollectionDaoInMemoryImpl.java
+++ src/main/java/beans/CollectionDaoInMemoryImpl.java
@@ -1,67 +1,67 @@
-package beans;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-public class CollectionDaoInMemoryImpl implements CollectionDao {
-	private Map<Long, Collection> collectionsMap = new HashMap<Long, Collection>();
-	{
-		Collection collection1 = new Collection (1L, "John", 1000);		
-		Collection collection2 = new Collection (2L, "Susan", 500);
-				
-		collectionsMap.put(collection1.getId(), collection1);
-		collectionsMap.put(collection2.getId(), collection2);
-	}
-	
-	public void insert(Collection collection) {
-		collectionsMap.put(collection.getId(), collection);
-	}
-	
-	public void update(Collection collection) {
-		collectionsMap.put(collection.getId(), collection);
-	}
-	
-	public void update(List<Collection>collections) {
-		for(Collection collection: collections) {
-			update(collection);
-		}
-	}
-	
-	public void delete(long collectionId) {
-		collectionsMap.remove(collectionId);
-	}
-	
-	public Collection find(long collectionId) {
-		return collectionsMap.get(collectionId);
-	}
-	
-	public List<Collection>find(List<Long>collectionIds){
-		List<Collection>collections = new ArrayList<Collection>();
-		for (Long id:collectionIds) {
-			collections.add(collectionsMap.get(id));
-		}
-		return collections;
-	}
-	
-	public List<Collection> find(String ownerName){
-		List<Collection> collections = new ArrayList<Collection>();
-		for (Collection collection:collectionsMap.values()) {
-			if(ownerName.equals(collection.getCollectionOwner())) {
-				collections.add(collection);
-			}
-		}
-		return collections;
-	}
-	
-	public List<Collection> find(boolean locked){
-		List<Collection> collections = new ArrayList<Collection>();
-		for (Collection collection: collectionsMap.values()) {
-			if(locked == (collection.isLocked())) {
-				collections.add(collection);
-			}
-		}
-		return collections;
-	}
-}
+//package beans;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class CollectionDaoInMemoryImpl implements CollectionDao {
+//	private Map<Long, Collection> collectionsMap = new HashMap<Long, Collection>();
+//	{
+//		Collection collection1 = new Collection (1L, "John", 1000);		
+//		Collection collection2 = new Collection (2L, "Susan", 500);
+//				
+//		collectionsMap.put(collection1.getId(), collection1);
+//		collectionsMap.put(collection2.getId(), collection2);
+//	}
+//	
+//	public void insert(Collection collection) {
+//		collectionsMap.put(collection.getId(), collection);
+//	}
+//	
+//	public void update(Collection collection) {
+//		collectionsMap.put(collection.getId(), collection);
+//	}
+//	
+//	public void update(List<Collection>collections) {
+//		for(Collection collection: collections) {
+//			update(collection);
+//		}
+//	}
+//	
+//	public void delete(long collectionId) {
+//		collectionsMap.remove(collectionId);
+//	}
+//	
+//	public Collection find(long collectionId) {
+//		return collectionsMap.get(collectionId);
+//	}
+//	
+//	public List<Collection>find(List<Long>collectionIds){
+//		List<Collection>collections = new ArrayList<Collection>();
+//		for (Long id:collectionIds) {
+//			collections.add(collectionsMap.get(id));
+//		}
+//		return collections;
+//	}
+//	
+//	public List<Collection> find(String ownerName){
+//		List<Collection> collections = new ArrayList<Collection>();
+//		for (Collection collection:collectionsMap.values()) {
+//			if(ownerName.equals(collection.getCollectionOwner())) {
+//				collections.add(collection);
+//			}
+//		}
+//		return collections;
+//	}
+//	
+//	public List<Collection> find(boolean locked){
+//		List<Collection> collections = new ArrayList<Collection>();
+//		for (Collection collection: collectionsMap.values()) {
+//			if(locked == (collection.isLocked())) {
+//				collections.add(collection);
+//			}
+//		}
+//		return collections;
+//	}
+//}
